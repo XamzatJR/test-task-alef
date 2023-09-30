989554038969734dd92663ccbb87414bc1a21ea9
XamzatJR <bahaev.hamzat95@gmail.com>
2023-09-30 22:40:33 +0300

    feat: add btn component

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-    <button>
-        <slot></slot>
-    </button>
+  <button v-bind="$attrs" class="py-2.5 px-5 text-sm font-normal flex items-center justify-center">
+    <slot name="icon" class="w-6 p-6 mr-1" />
+    <slot />
+  </button>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
