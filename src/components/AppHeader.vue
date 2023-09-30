bba0ace794180a2cb44d63b4081df2e319483ea4
XamzatJR <bahaev.hamzat95@gmail.com>
2023-09-30 22:57:15 +0300

    feat: add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
-
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <header>
-        <img src="/Frame 10.png" alt="logo">
-        <nav>
-            <RouterLink :to="{name: 'home'}">Форма</RouterLink>
-            <RouterLink :to="{name: 'preview'}">Превью</RouterLink>
-        </nav>
-    </header>        
+  <header class="w-full py-6 border-b-2 border-gray-200">
+    <div class="container mx-auto flex items-center">
+      <img class="header" src="/Frame 10.png" alt="logo">
+      <nav class="mx-auto flex gap-6">
+        <RouterLink class="text-sm text-black opacity-50 font-normal hover:opacity-100" :to="{ name: 'home' }">
+          Форма
+        </RouterLink>
+        <RouterLink class="text-sm text-black opacity-50 font-normal hover:opacity-100" :to="{ name: 'preview' }">
+          Превью
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
