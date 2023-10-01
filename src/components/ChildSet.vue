94c9911a6a141a9ce5da4e2be2e71926059a3d3f
XamzatJR <bahaev.hamzat95@gmail.com>
2023-10-01 17:36:48 +0300

    feat: add childSet component

---
+++ src/components/ChildSet.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import AppButton from './AppButton.vue'
+import AppInput from './AppInput.vue'
+
+defineProps<{
+  name: string
+  age: string
+}>()
+defineEmits<{
+  removeChild: [name: string]
+}>()
+</script>
+
+<template>
+  <fieldset class="flex gap-4">
+    <AppInput :id="name" :name="name" placeholder="Введите имя" label="Имя" />
+    <AppInput :id="age" :name="age" placeholder="Введите возраст" label="Возраст" />
+    <AppButton type="button" class="text-primary" @click="$emit('removeChild', name)">
+      Удалить
+    </AppButton>
+  </fieldset>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
