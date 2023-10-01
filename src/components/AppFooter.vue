2fee7f60c224557e4b3ee18e88e882fb3608112f
XamzatJR <bahaev.hamzat95@gmail.com>
2023-10-01 17:34:19 +0300

    feat: add footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="w-full flex justify-center items-center bg-graym mt-auto">
+    <div class="py-8">
+      all rights reserved
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
