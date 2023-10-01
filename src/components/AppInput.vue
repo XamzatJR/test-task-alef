b836576a99a5141c7252c49632c34063f3f3d335
XamzatJR <bahaev.hamzat95@gmail.com>
2023-10-01 17:35:39 +0300

    feat: add input component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
+
+<template>
+  <label class="block rounded-lg w-full px-4 py-2 border border-gray-200">
+    <div class="text-sm text-gray-400">
+      {{ label }}
+    </div>
+    <input class="block w-full text-sm font-normal text-black focus:outline-none placeholder:text-black" v-bind="$attrs" type="text">
+  </label>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
