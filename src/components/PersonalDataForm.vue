08dcc061e01ef9a7fadaccb248df9110a154c6b2
XamzatJR <bahaev.hamzat95@gmail.com>
2023-10-01 17:37:14 +0300

    feat: add form component

---
+++ src/components/PersonalDataForm.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import AppButton from './AppButton.vue'
+import AppInput from './AppInput.vue'
+import ChildSet from './ChildSet.vue'
+
+const childs = ref<{ name: string; age: string }[]>([])
+const MAX_CHILDS = 5
+
+function addChild() {
+  if (childs.value.length < MAX_CHILDS) {
+    childs.value.push(
+      {
+        name: `child-name-${Date.now()}`,
+        age: `child-age-${Date.now() + 1}`,
+      },
+    )
+  }
+}
+</script>
+
+<template>
+  <form action="#">
+    <h2 class="text-black text-base font-medium mb-5">
+      Персональные данные
+    </h2>
+    <div class="flex flex-col gap-2.5 w-full">
+      <AppInput placeholder="Введите имя" label="Имя" />
+      <AppInput placeholder="Введите возраст" label="Возраст" />
+    </div>
+    <section class="mt-11">
+      <div class="flex justify-between items-center">
+        <h2 class="text-black text-base font-medium">
+          Дети (макс. 5)
+        </h2>
+        <AppButton type="button" class="text-primary border-2 border-primary rounded-full" @click="addChild">
+          <template #icon>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="24px / plus">
+                <path id="Union" fill-rule="evenodd" clip-rule="evenodd" d="M5.13332 10.8556C4.50125 10.8555 3.98887 11.3679 3.98887 12C3.98887 12.6321 4.50126 13.1445 5.13332 13.1445L10.8554 13.1445L10.8554 18.8668C10.8554 19.4989 11.3678 20.0113 11.9999 20.0113C12.632 20.0113 13.1444 19.4989 13.1444 18.8668L13.1443 13.1445L18.8667 13.1445C19.4988 13.1445 20.0112 12.6321 20.0112 12C20.0112 11.3679 19.4988 10.8556 18.8667 10.8556L13.1443 10.8556L13.1443 5.13338C13.1443 4.50132 12.632 3.98893 11.9999 3.98893C11.3678 3.98893 10.8554 4.50131 10.8554 5.13338L10.8554 10.8556L5.13332 10.8556Z" fill="#01A7FD" />
+              </g>
+            </svg>
+          </template>
+          Добавить ребенка
+        </AppButton>
+      </div>
+      <div class="flex flex-col gap-2.5 mt-5">
+        <template v-if="childs.length">
+          <ChildSet v-for="child in childs" :key="child.name" :name="child.name" :age="child.age" />
+        </template>
+      </div>
+    </section>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
