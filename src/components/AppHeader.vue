fa3f1dcf00254e3a3cf729fd09b6e89f42cfe805
XamzatJR <bahaev.hamzat95@gmail.com>
2023-10-02 20:33:31 +0300

    refactor: change logo name

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,7 @@ import { RouterLink } from 'vue-router'
 <template>
   <header class="w-full py-6 px-4 border-b-2 border-gray-200">
     <div class="container mx-auto flex items-center">
-      <img class="header" src="/Frame 10.png" alt="logo">
+      <img class="header" src="/logo.png" alt="logo">
       <nav class="mx-auto flex gap-6">
         <RouterLink class="text-sm text-black opacity-50 font-normal hover:opacity-100" :to="{ name: 'home' }">
           Форма
