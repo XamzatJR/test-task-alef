42954a06b10fa30a8f80365da6efadcecf36b4ec
XamzatJR <bahaev.hamzat95@gmail.com>
2023-10-02 19:10:24 +0300

    feat: add PreviewElement component

---
+++ src/components/PreviewElement.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  age: string
+}>()
+</script>
+
+<template>
+  <div v-bind="$attrs" class="flex items-center justify-center text-base text-black  font-bold">
+    {{ name }}, {{ age }} лет
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
